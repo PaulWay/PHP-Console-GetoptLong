9d5adb266b7fcf8f08fb68dca609643dbe9becd5
Paul Wayper <paulway@mabula.net>
2012-02-29 00:13:23 +0000

    Added the generation of tests that check that the files used in these tests are also mentioned in the package.xml file.

--- tests/getopt_long_test.pl
+++ tests/getopt_long_test.pl
@@ -14,17 +14,49 @@ while (<>) {
     push @tests, \@args;
 }
 
+# Add tests to see that test files are in package.xml
+my %testprog_required;
+foreach my $aref (@tests) {
+    my $testprog = $$aref[1];
+    next if $testprog eq 'package.xml';
+    unless (exists $testprog_required{$testprog}) {
+        push @tests, [
+            "file test_$$aref[1] must be in package.xml",
+            'package.xml',
+            $testprog,
+            'yes',
+        ];
+        $testprog_required{$testprog} = 1;
+    }
+}
+# Now read through the package XML and find the mentions of test files,
+# saving them to a handy hash for later reference
+my %testprog_in_package;
+open my $fh, '<', '../package.xml' or die "Can't open ../package.xml: $!";
+while (<$fh>) {
+    $testprog_in_package{$1} = 'yes'
+     if m{<file baseinstalldir="/tests" name="tests/test_(\w+).php" role="test" />};
+}
+close $fh;
+print 'required: (',join(',',sort keys %testprog_required),'), in package: ('
+    , join(',',sort keys %testprog_in_package),")\n";
+
 # Test anywhere protocol:
 print "1..",scalar @tests, "\n";
 my $testno = 0;
 foreach my $aref (@tests) {
 	$testno++;
+    my $actual;
 	my ($description, $testprog, $args, $expected) = @$aref;
 	$args =~ s{([|!])}{\\$1}g; # escape pipes in args for test_setup
-	$expected =~ s{\\n}{\n}g; # turn \n into newlines for test_help
 	#print "php test_$testprog.php $args\n";
-    my $actual = qx{php test_$testprog.php $args};
-    chomp $actual;
+	if ($testprog eq 'package.xml') {
+	    $actual   = $testprog_in_package{$args} || 'no';
+	} else {
+    	$expected =~ s{\\n}{\n}g; # turn \n into newlines for test_help
+        $actual   = qx{php test_$testprog.php $args};
+        chomp $actual;
+    }
     if ($expected eq $actual) {
 		print "ok $testno $description.\n";
 	} else {
