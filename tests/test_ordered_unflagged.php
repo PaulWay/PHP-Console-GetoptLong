b3802adbe5592819097d238396b78f72d3573214
Paul Wayper <paulway@mabula.net>
2012-09-08 04:01:16 +0000

    Adding last ordered-unflagged option variable to tests.

--- tests/test_ordered_unflagged.php
+++ tests/test_ordered_unflagged.php
@@ -4,10 +4,12 @@ require_once "../Console/GetoptLong.php";
 
 $first = '';
 $second = '';
+$last = '';
 $args = Console_GetoptLong::getOptions(array(
 	'first|f|_1=s'  => &$first,
 	'second|s|_2:s' => &$second,
+	'last|l|_-1:s'  => &$last,
 ));
 
-echo "$first,$second,(", implode(',',$args), ")\n";
+echo "$first,$second,$last,(", implode(',',$args), ")\n";
 
